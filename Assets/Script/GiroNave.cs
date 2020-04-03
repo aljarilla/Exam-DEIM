0132f997889cde7ef395d0939350e955e5a7e8e1
aljarilla <aljarilla.jose1998@gmail.com>
2020-04-03 19:22:08 +0200

    Rotacion nave

---
+++ Assets/Script/GiroNave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroNave : MonoBehaviour
+{
+
+    public float torque;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque(transform.up * torque * turn);
+    }
+
+
+
+}
+
